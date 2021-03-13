8478d82e7c7add226e91bf75e17419f3d3bb6940
sanjanasri-v <80593342+sanjanasri-v@users.noreply.github.com>
2021-03-13 15:24:37 -0500

    Add files via upload

---
+++ bobalumath/src/main/java/com/bobalu/bobalumath/BobalumathApplication.java
@@ -0,0 +1,13 @@
+package com.bobalu.bobalumath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobalumathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobalumathApplication.class, args);
+	}
+
+}
---
+++ bobalumath/src/test/java/com/bobalu/bobalumath/BobalumathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobalu.bobalumath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobalumathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
